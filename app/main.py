67a71eb813edbc6ab07f6a184bbaf799fdf07e57
amir nik <amir.nik1996@protonmail.com>
2022-06-06 20:46:58 +0430

    added minimal home page

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from . import security
 from .routers import todos, users
@@ -8,3 +9,10 @@ app = FastAPI()
 app.include_router(security.router)
 app.include_router(users.router)
 app.include_router(todos.router)
+
+@app.get("/")
+def welcome():
+    html_content= """
+    <h1>Welcome! click <a href="/docs">here</a> for the documentation!</h1>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
